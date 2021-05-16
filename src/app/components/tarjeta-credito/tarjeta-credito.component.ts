cd0b4fd65a14c97891f1790c822c941bfdc37289
davidr123 <davidradaalejandro@gmail.com>
2021-05-16 18:12:18 -0500

    mi primer commit

---
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -0,0 +1,55 @@
+import { variable } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-tarjeta-credito',
+  templateUrl: './tarjeta-credito.component.html',
+  styleUrls: ['./tarjeta-credito.component.scss']
+})
+export class TarjetaCreditoComponent implements OnInit {
+
+
+  listTarjetas: any[]=[
+    {titular: 'Juan Perez', numeroTarjeta: '123213213213', fechaExpiracion: '11/21', cvv: '123'},
+    {titular: 'Miguel Gonzalez', numeroTarjeta: '23423432432423', fechaExpiracion: '11/22', cvv: '312'}
+  ];
+
+  form: FormGroup;
+
+
+  constructor(private fb: FormBuilder,
+    private toastr: ToastrService) {
+    this.form= this.fb.group({
+      titular: ['', Validators.required],
+      numeroTarjeta: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(8)]],
+      fechaExpiracion: ['', Validators.required, Validators.maxLength(5), Validators.minLength(5)],
+      cvv: ['', Validators.required, Validators.maxLength(3), Validators.minLength(3)]
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  agregarTarjeta(){
+    console.log(this.form);
+    const tarjeta: any= {
+titular: this.form.get('titular')?.value,
+numeroTarjeta: this.form.get('numeroTarjeta')?.value,
+fechaExpiracion: this.form.get('fechaExpiracion')?.value,
+cvv: this.form.get('cvv')?.value,
+
+    }
+    this.listTarjetas.push(tarjeta);
+    this.toastr.success('La tarjeta fue registrada con éxito!', 'Tarjeta registrada!');
+    this.form.reset();
+  }
+
+  eliminarTarjeta(index: number){
+    this.listTarjetas.splice(index, 1);
+    this.toastr.error('La tarjeta fue eliminada con éxito', 'tarjeta eliminada');
+
+  }
+
+}
